5d0d3e722099b6f9eb0d407f0c8feeb51c808681
Nifemi <deyeminifemi@gmail.com>
2022-01-23 18:05:53 +0100

    Made snake move Added edge detection Added direction change detection

--- src/App.js
+++ src/App.js
@@ -4,146 +4,138 @@ import Snake from './Components/Snake';
 import Fruit from './Fruit';
 
 
+const fruitCordinate = () => {
+  let x = Math.floor(Math.random() * 21);
+  let y = Math.floor(Math.random() * 21);
 
-const fruitCordinate = () =>{
- let x = Math.floor(Math.random() * 97);
- let y = Math.floor(Math.random() * 97);
-
- return([x,y])
+  return ([x, y])
 }
 
 
+class App extends Component {
 
+  state = {
+    score: 0,
+    speed: 700,
+    snakePosition: [[0, 0], [0, 1], [0,2]],
+    snakeFruit: fruitCordinate(),
+    direction: 'right'
 
-class App extends Component{
-  
-    state = {
-      score: 0,
-      speed: 700,
-      snakePosition: [[0,0],[0,4]],
-      snakeFruit: fruitCordinate(),
-      direction: 'right'
-       
-    }
-    
+  }
+
   componentDidMount() {
-   
-    setInterval(this.changeDirection, this.state.speed);
+    this.interval = setInterval(() => this.move(), this.state.speed)
     document.addEventListener('keydown', this.handleKeyDown);
-    
   }
 
+  getNewPosition(snakePosition, direction){
+    const snakeHead = snakePosition[snakePosition.length - 1];
 
-
-  // gameBoarder = () =>{
-  //   let snakePos = [...this.state.snakePosition];
-  //   let head = snakePos[snakePos.length - 1];
-  //   if(head[0]> 0 || head[1] > 100 || head[0] < 0 || head[1] < 100){
-  //     this.gameOver()
-  //   }
-  // }
-
-
-  enlargeSnake = () =>{
-    let snakePos = [...this.state.snakePosition];
-    let prev = snakePos[0];
-    snakePos.push(prev)
-    this.setState({snakePosition: snakePos})
+    switch (direction){
+      case "right":
+        return [snakeHead[0], snakeHead[1] + 1]
+      case "left":
+        return [snakeHead[0], snakeHead[1] - 1]
+      case "up":
+        return [snakeHead[0] - 1, snakeHead[1]]
+      case "down":
+        return [snakeHead[0] + 1, snakeHead[1]]
+    }
   }
 
-  fruitEaten = ()=>{
-   let snakePos = [...this.state.snakePosition];
-   let head = snakePos[snakePos.length - 1];
-   let food = this.state.snakeFruit;
-  //  console.log(head, food)
-   if((head[0] === food[0] && head[1] === food[1]) || (head[0] === (food[0]+1) && head[1] === (food[1]+1)) 
-   || (head[0] === (food[0]+2) && head[1] === (food[1]+2)) || (head[0] === (food[0]+3) && head[1] === (food[1]+3))){
-     console.log("done")
-     this.enlargeSnake();
-   }
+  move(){
+    this.setState(state => {
+      const newPosition = this.getNewPosition(state.snakePosition, state.direction);
+
+      if(newPosition[0] > 9 ||
+        newPosition[0] < 0 ||
+        newPosition[1] > 9 ||
+        newPosition[1] < 0
+      ){
+        //New node is invalid
+        return state;
+      }
+
+      return {
+        ...state,
+        snakePosition: [...state.snakePosition.slice(1), newPosition]
+      }
+    })
   }
 
-
+  //Checks when user goes in a direction
   handleKeyDown = (e) => {
-  
-    switch(e.keyCode) {
-    case  37: 
-     this.setState({direction: 'left'});
-     this.changeDirection()
-     this.fruitEaten()
-     break;
-     
-     case 38 : 
-     this.setState({direction: 'up'});
-     this.changeDirection()
-     this.fruitEaten()
-     break;
+    const isDirectionKey = [37, 38, 39, 40].includes(e.keyCode);
 
-     case 39 : 
-     this.setState({direction: 'right'});
-     this.changeDirection()
-     this.fruitEaten()
-     break;
+    if(isDirectionKey){
+      clearInterval(this.interval)
+    }
+
+    let newDirection;
+    let isReversed = false;
+    switch (e.keyCode) {
+      case  37:
+        newDirection = 'left';
+        isReversed = this.state.direction === 'right';
+
+        break;
+
+      case 38:
+        newDirection = 'up'
+        isReversed = this.state.direction === 'down'
+
+        break;
+
+      case 39 :
+        newDirection = 'right'
+        isReversed = this.state.direction === 'left'
+
+        break;
 
 
-     case 40: 
-     this.setState({direction: 'down'});
-     this.changeDirection()
-     this.fruitEaten()
-     break;
+      case 40:
+        newDirection = 'down'
+        isReversed = this.state.direction === 'up'
+
+        break;
+    }
+
+    if(isDirectionKey){
+      this.setState(state => {
+          let currentPositions = [...state.snakePosition];
+
+          if(isReversed){
+            currentPositions = currentPositions.reverse()
+          }
+
+          return {
+            ...state,
+            snakePosition: currentPositions,
+            direction: newDirection
+          }
+      }, () => {
+        this.move()
+
+        this.interval = setInterval(() => this.move(), this.state.speed)
+      })
+    }
   }
-  
-}
 
-
-
-
-
-changeDirection = () => {
-  let snakePos = [...this.state.snakePosition];
-  let snakeHead = snakePos[snakePos.length - 1];
-
-  switch(this.state.direction) {
-    case 'left': 
-    snakeHead = [snakeHead[0], snakeHead[1] - 4];
-    snakePos.splice(0,1);
-    break;
-    case 'up': 
-    snakeHead = [snakeHead[0] - 4, snakeHead[1]];
-    snakePos.splice(0,1);
-    break;
-    case 'right': 
-    snakeHead = [snakeHead[0], snakeHead[1] + 4];
-    snakePos.splice(0,1);
-    break;
-    case 'down': 
-    snakeHead = [snakeHead[0] + 4, snakeHead[1]];
-    snakePos.splice(0,1);
-    break;
-  }
-
-  snakePos.push(snakeHead);
-  this.setState({
-    snakePosition : snakePos
-  })
-}
-  
-
-render(){
-  return(
-    <div className='App'>      
-      <main className='snake-board'>
-       <div className='container'>
-          <div className='snake-field'>
-             <Snake snakePosition={this.state.snakePosition} />
-             <Fruit fruit={this.state.snakeFruit} />
+  render() {
+    return (
+      <div className='App'>
+        <main className='snake-board'>
+          <div className='container'>
+            <div className='snake-field'>
+              <Snake snakePosition={this.state.snakePosition}/>
+              {/*<Fruit fruit={this.state.snakeFruit}/>*/}
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
 
-  );
- }
+    );
+  }
 }
 
 export default App;
