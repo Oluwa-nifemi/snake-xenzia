7a329f66507a977cfc67ec647250d90b61168d50
Nifemi <deyeminifemi@gmail.com>
2022-01-26 20:54:41 +0100

    Detect snake eating itself

--- src/App.js
+++ src/App.js
@@ -16,16 +16,23 @@ const generateRandomFruit = () => {
 }
 
 
+const initialState = {
+  score: 0,
+  speed: 400,
+  snakePosition: [[0, 0], [0, 1], [0,2]],
+  snakePositionMap: {
+    '0,0': true,
+    '0,1': true,
+    '0,2': true,
+  },
+  snakeFruit: generateRandomFruit(),
+  direction: 'right'
+
+}
+
 class App extends Component {
 
-  state = {
-    score: 0,
-    speed: 700,
-    snakePosition: [[0, 0], [0, 1], [0,2]],
-    snakeFruit: generateRandomFruit(),
-    direction: 'right'
-
-  }
+  state = initialState
 
   componentDidMount() {
     this.interval = setInterval(() => this.move(), this.state.speed)
@@ -60,19 +67,35 @@ class App extends Component {
         return state;
       }
 
+      if(this.state.snakePositionMap[`${newPosition[0]},${newPosition[1]}`]){
+        alert('Game over stop eating yourself')
+        return Object.assign(initialState)
+      }
+
+      //Add new position to snake position map
+      const newPositionMap = {...this.state.snakePositionMap};
+
+      newPositionMap[`${newPosition[0]},${newPosition[1]}`] = true;
+
       //Check if head matches the snake position
       if(newPosition[0] === this.state.snakeFruit[0] && newPosition[1] === this.state.snakeFruit[1]){
+
         //If head matches snake position then do not remove tail
         return {
           ...state,
           snakePosition: [...state.snakePosition, newPosition],
+          snakePositionMap: newPositionMap,
           snakeFruit: generateRandomFruit() //Generate new position for snake
         }
       }
 
+      const [firstX, firstY] = state.snakePosition[0];
+      newPositionMap[`${firstX},${firstY}`] = false;
+
       return {
         ...state,
-        snakePosition: [...state.snakePosition.slice(1), newPosition]
+        snakePosition: [...state.snakePosition.slice(1), newPosition],
+        snakePositionMap: newPositionMap
       }
     })
   }
